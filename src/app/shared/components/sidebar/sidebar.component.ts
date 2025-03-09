e2c94e2eeae6c73172ea9a6bb90e3a30a7493765
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2025-03-09 10:51:24 +0700

    feat: add sidebar component

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  imports: [RouterModule],
+})
+export class SidebarComponent {}
