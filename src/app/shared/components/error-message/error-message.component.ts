9f40091840171b229245abc6b92ed3a7b3f7ea15
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2025-03-09 22:36:09 +0700

    feat: add error message component

---
+++ src/app/shared/components/error-message/error-message.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-message',
+  templateUrl: './error-message.component.html',
+})
+export class ErrorMessageComponent {}
